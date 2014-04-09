6dd496a25ec3380ef88e7d502440ee469966f271
jngrt <jangeertmunneke@gmail.com>
2014-04-09 16:58:36 +0200

    text changes

--- js/main.js
+++ js/main.js
@@ -1,29 +1,4 @@
 $(function(){
-  /*
-  * Gauge
-  */
-  // var opts = {
-  //   lines: 20, // The number of lines to draw
-  //   angle: 0, // The length of each line
-  //   lineWidth: 0.15, // The line thickness
-  //   pointer: {
-  //     length: 0.9, // The radius of the inner circle
-  //     strokeWidth: 0.045, // The rotation offset
-  //     color: '#000000' // Fill color
-  //   },
-  //   limitMax: 'true',   // If true, the pointer will not go past the end of the gauge
-  //   colorStart: '#6FADCF',   // Colors
-  //   colorStop: '#8FC0DA',    // just experiment with them
-  //   strokeColor: '#EBEDEF',   // to see which ones work best for you
-  //   generateGradient: true
-  // };
-  // var target = document.getElementById('gauge'); // your canvas element
-  // var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
-  // gauge.maxValue = 3000; // set max gauge value
-  // gauge.animationSpeed = 32; // set animation speed (32 is default value)
-  // gauge.set(1650); // set actual value
-
-
 
   $('#gaugeContainer').jqxGauge({
     min: 0,
